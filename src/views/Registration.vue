e8ad14528de1127a67444c5a4eadaceafb8a85b4
FadhilP <fadhilpradipta0@gmail.com>
2022-05-17 13:03:59 +0700

    Update whatsapp field

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,36 +1,38 @@
 <template>
-	<div class="font-sans min-h-screen">
-		<div class="flex flex-row justify-center">
-			<img
-				class="h-6 mt-5 mb-10 lg:my-6 object-contain"
-				src="../assets/logo.png"
-				alt=""
-			/>
-		</div>
-		<div class="border rounded-lg w-10/12 sm:w-8/12 lg:w-4/12 mx-auto mb-8">
-			<div class="flex-wrap my-8">
-				<p class="text-center text-lg font-semibold text-gray-600 my-1">
-					Satu langkah lagi
-				</p>
-				<p class="text-center text-sm text-gray-600 mx-8">
-					Mohon untuk mengisi data diri Anda terlebih dahulu, agar kami dapat
-					memberikan bantuan dan informasi layanan secara optimal
-				</p>
-			</div>
-			<div class="w-11/12 border-t border-gray-300 mx-auto"></div>
-			<form @submit.prevent="submit">
-				<div class="flex-wrap my-8 mx-8">
-					<div class="block">
-						<p class="text-gray-600 font-semibold text-sm">Nama</p>
-						<input
-							v-model.lazy="person.name"
-							class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
-							placeholder="Walter Joseph Kovacs"
-							value=""
-						/>
-						<p class="text-red-500 font-semibold text-sm" v-if="errors.name">{{errors.name}}</p>
-					</div>
-					<!-- <div class="block mt-4">
+  <div class="font-sans min-h-screen">
+    <div class="flex flex-row justify-center">
+      <img
+        class="h-6 mt-5 mb-10 lg:my-6 object-contain"
+        src="../assets/logo.png"
+        alt=""
+      />
+    </div>
+    <div class="border rounded-lg w-10/12 sm:w-8/12 lg:w-4/12 mx-auto mb-8">
+      <div class="flex-wrap my-8">
+        <p class="text-center text-lg font-semibold text-gray-600 my-1">
+          Satu langkah lagi
+        </p>
+        <p class="text-center text-sm text-gray-600 mx-8">
+          Mohon untuk mengisi data diri Anda terlebih dahulu, agar kami dapat
+          memberikan bantuan dan informasi layanan secara optimal
+        </p>
+      </div>
+      <div class="w-11/12 border-t border-gray-300 mx-auto"></div>
+      <form @submit.prevent="submit">
+        <div class="flex-wrap my-8 mx-8">
+          <div class="block">
+            <p class="text-gray-600 font-semibold text-sm">Nama</p>
+            <input
+              v-model.lazy="person.name"
+              class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
+              placeholder="Walter Joseph Kovacs"
+              value=""
+            />
+            <p class="text-red-500 font-semibold text-sm" v-if="errors.name">
+              {{ errors.name }}
+            </p>
+          </div>
+          <!-- <div class="block mt-4">
 						<p class="text-gray-600 font-semibold text-sm">
 							Nomor Induk Kependudukan
 						</p>
@@ -41,7 +43,7 @@
 							value=""
 						/>
 					</div> -->
-					<!-- <div class="block mt-4">
+          <!-- <div class="block mt-4">
 						<p class="text-gray-600 font-semibold text-sm">Jenis Kelamin</p>
 						<div class="mt-2">
 							<label
@@ -74,7 +76,7 @@
 							</label>
 						</div>
 					</div> -->
-					<!-- <div class="block mt-4">
+          <!-- <div class="block mt-4">
 						<p class="text-gray-600 font-semibold text-sm">Nomor Telepon</p>
 						<input
 							v-model.lazy="person.phone"
@@ -84,98 +86,110 @@
 						/>
 					</div> -->
 
-					<!-- JABATAN INPUT -->
-					<div class="block mt-4">
-						<p class="text-gray-600 font-semibold text-sm">Jabatan</p>
-						<select
-							class="form-select mt-1 block w-full text-sm capitalize focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
-							:class="inputStyle(jabatan)"
-							v-model="person.jabatan"
-							placeholder="No"
-						>
-							<option value="" disabled selected>Pilih Jabatan</option>
-							<option
-								v-for="(data, index) in jabatan"
-								:value="data"
-								:key="`jabatan${index}`"
-								>{{ data }}</option
-							>
-						</select>
-						<p class="text-red-500 font-semibold text-sm" v-if="errors.jabatan">{{errors.jabatan}}</p>
-					</div>
+          <!-- JABATAN INPUT -->
+          <div class="block mt-4">
+            <p class="text-gray-600 font-semibold text-sm">Jabatan</p>
+            <select
+              class="form-select mt-1 block w-full text-sm capitalize focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
+              :class="inputStyle(jabatan)"
+              v-model="person.jabatan"
+              placeholder="No"
+            >
+              <option value="" disabled selected>Pilih Jabatan</option>
+              <option
+                v-for="(data, index) in jabatan"
+                :value="data"
+                :key="`jabatan${index}`"
+                >{{ data }}</option
+              >
+            </select>
+            <p class="text-red-500 font-semibold text-sm" v-if="errors.jabatan">
+              {{ errors.jabatan }}
+            </p>
+          </div>
 
-					<div v-if="person.jabatan === 'Lainnya'" class="block mt-4">
-						<p class="text-gray-600 font-semibold text-sm">
-							Masukkan Jabatan Anda
-						</p>
-						<input
-							v-model.lazy="person.jabatanLainnya"
-							class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
-							placeholder="Jabatan di perusahaan"
-							value=""
-						/>
-					</div>
-					<!-- JABATAN INPUT END -->
+          <div v-if="person.jabatan === 'Lainnya'" class="block mt-4">
+            <p class="text-gray-600 font-semibold text-sm">
+              Masukkan Jabatan Anda
+            </p>
+            <input
+              v-model.lazy="person.jabatanLainnya"
+              class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
+              placeholder="Jabatan di perusahaan"
+              value=""
+            />
+          </div>
+          <!-- JABATAN INPUT END -->
 
-					<!-- UNIT KERJA INPUT -->
-					<div class="block mt-4">
-						<p class="text-gray-600 font-semibold text-sm">Unit Kerja</p>
-						<select
-							class="form-select mt-1 block w-full text-sm capitalize focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
-							:class="inputStyle(unitKerja)"
-							v-model="person.unitKerja"
-							placeholder="No"
-						>
-							<option value="" disabled selected>Pilih unit kerja</option>
-							<option
-								v-for="(data, index) in unitKerja"
-								:value="data"
-								:key="`unitKerja${index}`"
-								>{{ data }}</option
-							>
-						</select>
-						<p class="text-red-500 font-semibold text-sm" v-if="errors.unitKerja">{{errors.unitKerja}}</p>
-					</div>
+          <!-- UNIT KERJA INPUT -->
+          <div class="block mt-4">
+            <p class="text-gray-600 font-semibold text-sm">Unit Kerja</p>
+            <select
+              class="form-select mt-1 block w-full text-sm capitalize focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
+              :class="inputStyle(unitKerja)"
+              v-model="person.unitKerja"
+              placeholder="No"
+            >
+              <option value="" disabled selected>Pilih unit kerja</option>
+              <option
+                v-for="(data, index) in unitKerja"
+                :value="data"
+                :key="`unitKerja${index}`"
+                >{{ data }}</option
+              >
+            </select>
+            <p
+              class="text-red-500 font-semibold text-sm"
+              v-if="errors.unitKerja"
+            >
+              {{ errors.unitKerja }}
+            </p>
+          </div>
 
-					<div v-if="person.unitKerja === 'Lainnya'" class="block mt-4">
-						<p class="text-gray-600 font-semibold text-sm">
-							Masukkan Unit Kerja Anda
-						</p>
-						<input
-							v-model.lazy="person.unitKerjaLainnya"
-							class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
-							placeholder="Unit kerja di perusahaan"
-							value=""
-						/>
-					</div>
-					<!-- JABATAN INPUT END -->
+          <div v-if="person.unitKerja === 'Lainnya'" class="block mt-4">
+            <p class="text-gray-600 font-semibold text-sm">
+              Masukkan Unit Kerja Anda
+            </p>
+            <input
+              v-model.lazy="person.unitKerjaLainnya"
+              class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
+              placeholder="Unit kerja di perusahaan"
+              value=""
+            />
+          </div>
+          <!-- JABATAN INPUT END -->
 
-					<div class="block mt-4">
-						<p class="text-gray-600 font-semibold text-sm">
-							Nomor WhatsApp
-						</p>
-						<input
-							v-model.lazy="person.whatsAppNo"
-							class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
-							placeholder="Nomor WhatsApp"
-							value=""
-							type="number"
-						/>
-						<p class="text-red-500 font-semibold text-sm" v-if="errors.whatsAppNo">{{errors.whatsAppNo}}</p>
-					</div>
+          <div class="block mt-4">
+            <p class="text-gray-600 font-semibold text-sm">
+              Nomor WhatsApp
+            </p>
+            <input
+              v-model.lazy="person.whatsAppNo"
+              class="form-input mt-2 block w-full text-sm focus:bg-gray-200 text-gray-800 placeholder-gray-500 focus:shadow-none focus:border-transparent focus:outline-none"
+              placeholder="Nomor WhatsApp"
+              value=""
+              @keypress="isNumber($event)"
+            />
+            <p
+              class="text-red-500 font-semibold text-sm"
+              v-if="errors.whatsAppNo"
+            >
+              {{ errors.whatsAppNo }}
+            </p>
+          </div>
 
-					<div class="block mt-8">
-						<v-button
-							type="submit"
-							msg="Selanjutnya"
-							class="py-3"
-							:class="loadingClasses"
-						/>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
+          <div class="block mt-8">
+            <v-button
+              type="submit"
+              msg="Selanjutnya"
+              class="py-3"
+              :class="loadingClasses"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 <script>
 import Vue from "vue";
@@ -184,140 +198,151 @@ import VCalendar from "v-calendar";
 import Button from "@/components/Button.vue";
 
 Vue.use(VCalendar, {
-	locales: {
-		"id-ID": {
-			masks: {
-				L: "YYYY-MM-DD",
-				// ...optional `title`, `weekdays`, `navMonths`, etc
-			},
-		},
-	},
+  locales: {
+    "id-ID": {
+      masks: {
+        L: "YYYY-MM-DD",
+        // ...optional `title`, `weekdays`, `navMonths`, etc
+      },
+    },
+  },
 });
 
 export default {
-	name: "test",
-	components: {
-		"v-button": Button,
-	},
-	data: function() {
-		return {
-			username: "",
-			checkTokenAPI: process.env.VUE_APP_CHECK_TOKEN_API,
-			registrationAPI: `${process.env.VUE_APP_API_URL}/user-company-detail/store`,
-			jabatan: [
-				"Staff / Junior level",
-				"Supervisor / Senior level",
-				"Manager",
-				"General Manager",
-				"Direktur",
-				"Lainnya",
-			],
-			unitKerja: [
-				"Produksi",
-				"Pemasaran",
-				"Keuangan",
-				"Pengadaan",
-				"Teknologi Informasi (IT)",
-				"Lainnya",
-			],
-			kodePerusahaan: null,
-			person: {
-				name: "",
-				jabatan: "",
-				unitKerja: "",
-				jabatanLainnya: null,
-				unitKerjaLainnya: null,
-				whatsAppNo: ""
-			},
-			key: "",
-			errors: {
-				name: "",
-				jabatan: "",
-				unitKerja: "",
-				whatsAppNo: "",
-			}
-		};
-	},
-	/* eslint-disable no-console */
-	async created() {},
-	async mounted() {
-		window.location.href = "#";
-		this.username = localStorage.getItem("username");
-		this.person.name = this.username;
-		this.user_id = localStorage.getItem("identifier");
-		this.kodePerusahaan = localStorage.getItem("kodePerusahaan");
-	},
-	methods: {
-		/* eslint-disable no-console */
-		submit: function() {
-			this.errors = {	
-				name: "",
-				jabatan: "",
-				unitKerja: "",
-				whatsAppNo: ""
-			}
-			if (!this.person.jabatan) this.errors['jabatan'] = "Harap memasukkan jabatan"
-			if (!this.person.unitKerja) this.errors['unitKerja'] = "Harap memasukkan unit kerja"
-			if (!this.person.name) this.errors['name'] = "Harap memasukkan nama"
-			if (!this.person.whatsAppNo) this.errors['whatsAppNo'] = "Harap memasukkan nomor whatsapp"
-			
-			
-			// Jabatan Handler
-			let jabatanValue = null;
-			if (this.person.jabatan === "Lainnya") {
-				jabatanValue = this.person.jabatanLainnya;
-			} else jabatanValue = this.person.jabatan;
+  name: "test",
+  components: {
+    "v-button": Button,
+  },
+  data: function() {
+    return {
+      username: "",
+      checkTokenAPI: process.env.VUE_APP_CHECK_TOKEN_API,
+      registrationAPI: `${process.env.VUE_APP_API_URL}/user-company-detail/store`,
+      jabatan: [
+        "Staff / Junior level",
+        "Supervisor / Senior level",
+        "Manager",
+        "General Manager",
+        "Direktur",
+        "Lainnya",
+      ],
+      unitKerja: [
+        "Produksi",
+        "Pemasaran",
+        "Keuangan",
+        "Pengadaan",
+        "Teknologi Informasi (IT)",
+        "Lainnya",
+      ],
+      kodePerusahaan: null,
+      person: {
+        name: "",
+        jabatan: "",
+        unitKerja: "",
+        jabatanLainnya: null,
+        unitKerjaLainnya: null,
+        whatsAppNo: "",
+      },
+      key: "",
+      errors: {
+        name: "",
+        jabatan: "",
+        unitKerja: "",
+        whatsAppNo: "",
+      },
+    };
+  },
+  /* eslint-disable no-console */
+  async created() {},
+  async mounted() {
+    window.location.href = "#";
+    this.username = localStorage.getItem("username");
+    this.person.name = this.username;
+    this.user_id = localStorage.getItem("identifier");
+    this.kodePerusahaan = localStorage.getItem("kodePerusahaan");
+  },
+  methods: {
+    /* eslint-disable no-console */
+    submit: function() {
+      this.errors = {
+        name: "",
+        jabatan: "",
+        unitKerja: "",
+        whatsAppNo: "",
+      };
+      if (!this.person.jabatan)
+        this.errors["jabatan"] = "Harap memasukkan jabatan";
+      if (!this.person.unitKerja)
+        this.errors["unitKerja"] = "Harap memasukkan unit kerja";
+      if (!this.person.name) this.errors["name"] = "Harap memasukkan nama";
+      if (!this.person.whatsAppNo)
+        this.errors["whatsAppNo"] = "Harap memasukkan nomor whatsapp";
 
-			// Unit Kerja Handler
-			let unitKerjaValue = null;
-			if (this.person.unitKerja === "Lainnya") {
-				unitKerjaValue = this.person.unitKerjaLainnya;
-			} else unitKerjaValue = this.person.unitKerja;
+      // Jabatan Handler
+      let jabatanValue = null;
+      if (this.person.jabatan === "Lainnya") {
+        jabatanValue = this.person.jabatanLainnya;
+      } else jabatanValue = this.person.jabatan;
 
-			axios
-				.post(
-					this.registrationAPI,
-					{
-						companyCode: this.kodePerusahaan,
-						division: unitKerjaValue,
-						position: jabatanValue,
-						whatsAppNo: this.person.whatsAppNo
-					},
-					{
-						headers: {
-							Authorization: `${localStorage.getItem("token")}`,
-						},
-					}
-				)
-				.then(
-					(response) => (
-						(this.output = response.data), this.$router.push({ name: "intro" })
-					),
-					(error) => {
-						console.log(error.response.data.error.name);
-						if (error.response.data.error.name === "InvalidJwtToken")
-							this.$router.push({ name: "home" });
-					}
-				);
-		},
+      // Unit Kerja Handler
+      let unitKerjaValue = null;
+      if (this.person.unitKerja === "Lainnya") {
+        unitKerjaValue = this.person.unitKerjaLainnya;
+      } else unitKerjaValue = this.person.unitKerja;
 
-		inputStyle: function(targetInput) {
-			// bind with one method and return Array
-			return [targetInput == "" ? "text-gray-500" : "text-gray-800"];
-		},
-		toggleLoading: function() {
-			this.isLoading = !this.isLoading;
-		},
-	},
-	computed: {
-		loadingClasses: function() {
-			return {
-				"spinner cursor-wait": this.isLoading,
-				"": !this.isLoading,
-			};
-		},
-	},
-	/* eslint-enable no-console */
+      axios
+        .post(
+          this.registrationAPI,
+          {
+            companyCode: this.kodePerusahaan,
+            division: unitKerjaValue,
+            position: jabatanValue,
+            whatsAppNo: this.person.whatsAppNo,
+          },
+          {
+            headers: {
+              Authorization: `${localStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then(
+          (response) => (
+            (this.output = response.data), this.$router.push({ name: "intro" })
+          ),
+          (error) => {
+            console.log(error.response.data.error.name);
+            if (error.response.data.error.name === "InvalidJwtToken")
+              this.$router.push({ name: "home" });
+          }
+        );
+    },
+
+    inputStyle: function(targetInput) {
+      // bind with one method and return Array
+      return [targetInput == "" ? "text-gray-500" : "text-gray-800"];
+    },
+    toggleLoading: function() {
+      this.isLoading = !this.isLoading;
+    },
+    isNumber: function(evt) {
+      evt = evt ? evt : window.event;
+      var charCode = evt.which ? evt.which : evt.keyCode;
+      if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        evt.preventDefault();
+      } else {
+        return true;
+      }
+    },
+  },
+  computed: {
+    loadingClasses: function() {
+      return {
+        "spinner cursor-wait": this.isLoading,
+        "": !this.isLoading,
+      };
+    },
+  },
+  /* eslint-enable no-console */
 };
 </script>
 
@@ -331,11 +356,11 @@ input[type="button"]::-moz-focus-inner,
 input[type="submit"]::-moz-focus-inner,
 select::-moz-focus-inner,
 input[type="file"] > input[type="button"]::-moz-focus-inner {
-	outline: none !important;
+  outline: none !important;
 }
 
 select:-moz-focusring {
-	color: transparent;
-	text-shadow: 0 0 0 #000;
+  color: transparent;
+  text-shadow: 0 0 0 #000;
 }
 </style>
